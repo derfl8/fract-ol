89d39d98eea5f0b242c7dd79ad485887da706e3e
derfl8 <alfred.begou@gmail.com>
2026-03-03 20:08:41 +0100

    opti zoom

--- more_events.c
+++ more_events.c
@@ -6,7 +6,7 @@
 /*   By: abegou <abegou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/24 13:40:57 by abegou            #+#    #+#             */
-/*   Updated: 2026/03/03 19:59:50 by abegou           ###   ########.fr       */
+/*   Updated: 2026/03/03 20:07:27 by abegou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,26 +26,20 @@ static double	zoom_max(double min, double max, double factor)
 void mouse_wheel_hook(int button, void* param)
 {
 	t_env	*env;
-	double temp;
+	double	temp;
+	double	factor;
 
+	factor = 1;
 	env = (t_env *)param;
 	if (button == 1)
-    {
-		temp = zoom_min(env->min_x, env->max_x, 0.9);
-	    env->max_x = zoom_max(env->min_x, env->max_x, 0.9);
-		env->min_x = temp;
-		temp = zoom_min(env->min_y, env->max_y, 0.9);
-	    env->max_y = zoom_max(env->min_y, env->max_y, 0.9);
-		env->min_y = temp;
-    }
-    if (button == 2)
-    {
-		temp = zoom_min(env->min_x, env->max_x, 1.1);
-	    env->max_x = zoom_max(env->min_x, env->max_x, 1.1);
-		env->min_x = temp;
-		temp = zoom_min(env->min_y, env->max_y, 1.1);
-	    env->max_y = zoom_max(env->min_y, env->max_y, 1.1);
-		env->min_y = temp;
-    }
+		factor = 0.9;
+    else if (button == 2)
+		factor = 1.1;
+	temp = zoom_min(env->min_x, env->max_x, factor);
+	env->max_x = zoom_max(env->min_x, env->max_x, factor);
+	env->min_x = temp;
+	temp = zoom_min(env->min_y, env->max_y, factor);
+	env->max_y = zoom_max(env->min_y, env->max_y, factor);
+	env->min_y = temp;
 	render_mandelbrot(&env);
 }
